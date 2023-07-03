dfc0164d09d52fa1d7348fc7c2c9b779aee33573
ahus2503 <ahus21024@gmail.com>
2023-07-03 15:49:41 +0300

    initial comment

---
+++ h1/hello.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+console.log("hello")
+let userName // variable declaration
+userName = "kelvin" // variable assignment
+console.log(userName)
+userName = "orange"
+console.log(userName)
+
+let name = "hi" 
+console.log(name)
+let namE= "hey"
+console.log(namE)
---
+++ h1/hello2.js
@@ -0,0 +1,16 @@
+let firstNumber
+firstNumber = 5
+
+let secondNumber = 15
+
+const pie = 3.142
+
+let sum = firstNumber + secondNumber
+
+console.log(sum)
+
+firstNumber = 123
+sum = firstNumber + secondNumber
+console.log(sum)
+
+pie = 3.143
